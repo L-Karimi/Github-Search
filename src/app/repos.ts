350f7f0f9e91c0eb863491666123350d8ec80c95
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-14 17:43:28 +0300

    declare variables

---
+++ src/app/repos.ts
@@ -0,0 +1,9 @@
+export class Repos {
+    constructor(
+    public html_url:string,
+    public name:string,
+    public description:string,
+     public language:string,
+    public created_at: Date
+    ) {}
+}
