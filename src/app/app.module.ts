ca1536851367f99f301ecf90ab289b1abc1f846a
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-14 17:51:25 +0300

    changes in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReposComponent } from './repos/repos.component';
 import { SearchComponent } from './search/search.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FilterPipe } from './filter.pipe';
+import {HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
@@ -20,7 +22,10 @@ import { FilterPipe } from './filter.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    // NgProgressModule.forRoot(),
+    // NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
