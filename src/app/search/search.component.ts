082e012461f6e69a31ab115b5d379c789c21a1c1
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-14 17:49:33 +0300

    add a function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,11 @@
+import { StaticSymbol } from '@angular/compiler';
+import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
+
+
+
 import { Component, OnInit } from '@angular/core';
+import {ProfileService}from '../profile.service'
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +13,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  search: any[]= [];
 
-  constructor() { }
+  constructor(public pservices: ProfileService) { }
 
   ngOnInit(): void {
+    this.pservices.getGithubUsers("luciahroyalty101")
+    .subscribe((response: any)=>
+    this.search=response.items)
   }
 
 }
