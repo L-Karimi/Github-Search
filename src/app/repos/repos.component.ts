0be27cfdc147b7c9bc066ceb39aad0363a41b6d7
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-15 18:50:24 +0300

    add constructor method and specify data types

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,7 +1,9 @@
 import { StaticSymbol } from '@angular/compiler';
 import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
+// import { profile } from 'console';
 import { ProfileService} from '../profile.service'
+import {Search} from '../search'
 
 @Component({
   selector: 'app-repos',
@@ -9,17 +11,23 @@ import { ProfileService} from '../profile.service'
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
-
-  repos: any[]= [];
+  repos: any;
+  user: any;
+  username:any;
 
   constructor(public pservices: ProfileService) { }
-    // username='name'
-    // Repositories='repos'
+    getUserProfile(user){
+      this.pservices.getGithubUsers(user).subscribe(Response=>{
+        this.user=Response;
+      });
+      this.pservices.getGithubRepositories(user).subscribe(repos=>{
+        this.repos=repos;
+      });
+    }
  
   ngOnInit(): void {
-      this.pservices.getGithubRepositories("lucy")
-      .subscribe((response: any)=>
-      this.repos=response.items)
-  }
 
+this.getUserProfile('luciahroyalty101');
+  }
 }
+
