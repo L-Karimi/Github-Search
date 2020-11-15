16964e97747f2f6e8a5cdcba0211df4648ad5534
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-15 18:51:45 +0300

    imput search button functionality

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,26 +1,30 @@
-import { StaticSymbol } from '@angular/compiler';
-import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
+// import { StaticSymbol } from '@angular/compiler';
+// import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
 
-
-
-import { Component, OnInit } from '@angular/core';
-import {ProfileService}from '../profile.service'
+import { Component,EventEmitter,Output, OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'app-search',
-  templateUrl:'./search.component.html',
+  templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  search: any[]= [];
+searchGithubUser:any
+@Output() searchGitUser = new EventEmitter<any>();
 
-  constructor(public pservices: ProfileService) { }
+searchUser(){
+  this.searchGitUser.emit(this.searchGithubUser)
+}
+
+
+
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.pservices.getGithubUsers("luciahroyalty101")
-    .subscribe((response: any)=>
-    this.search=response.items)
-  }
+  
+     }
 
-}
+    }
+  
