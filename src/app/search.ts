afd1385d797ba92771fbaaf52275bdf27fc207c3
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-15 18:50:46 +0300

    modify file

--- src/app/search.ts
+++ src/app/search.ts
@@ -1,7 +1,15 @@
 export class Search {
-    public find:string;
-    constructor(find){
-        this.find=find
-    }
+    // public find:string;
+    // constructor(
+    //     public login:string,
+    //     public_repos:Number,
+    //    public followers:Number,
+    //     public following:Number,
+    //    public created_at:Number,
+    // ){
+    //     // this.find=find
+        
+
+    // }
 }
 
