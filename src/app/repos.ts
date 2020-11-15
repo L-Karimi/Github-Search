88032ea059c29316a421cc0349b23e1ebc530f7f
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-15 18:53:11 +0300

    export class search

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -4,6 +4,19 @@ export class Repos {
     public name:string,
     public description:string,
      public language:string,
-    public created_at: Date
+     public created_at: Date
+     
     ) {}
 }
+export class Search{
+    constructor(
+        
+        public login:string,
+        public_repos:Number,
+       public followers:Number,
+        public following:Number,
+       public created_at:Number,
+    
+    ) {}
+}
+    
