4c7460fce0edd378e40ec1922932ea02181e017f
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-16 13:16:05 +0300

    add constant routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReposComponent } from './repos/repos.component';
 import { SearchComponent } from './search/search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 const routes: Routes = [
+  { path: '', redirectTo:"/repos", pathMatch:"full"},
   { path: 'repos', component: ReposComponent },
   { path: 'search', component: SearchComponent },
+  { path:'**', component:NotFoundComponent},
+  
 ];
 
 @NgModule({
