6b3ca3b82d01dae07fd6a93843fa643269644c1d
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-16 13:20:03 +0300

    add eventemitter

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 // import { StaticSymbol } from '@angular/compiler';
 // import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
 
-import { Component,EventEmitter,Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 
 
 @Component({
@@ -10,21 +10,21 @@ import { Component,EventEmitter,Output, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-searchGithubUser:any
-@Output() searchGitUser = new EventEmitter<any>();
+  searchGithubUser: any
+  @Output() searchGitUser = new EventEmitter<any>();
 
-searchUser(){
-  this.searchGitUser.emit(this.searchGithubUser)
-}
-
-
+  searchUser() {
+    this.searchGitUser.emit(this.searchGithubUser)
+  }
 
 
-  constructor() {}
+
+
+  constructor() { }
 
   ngOnInit(): void {
-  
-     }
 
-    }
-  
+  }
+
+}
+
